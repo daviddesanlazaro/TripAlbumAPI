a57760d4b3be81f6c7a6a84f1f34407ab3045d97
David de San Lázaro <david.dslazaro@gmail.com>
2021-11-24 13:48:54 +0100

    Creación del proyecto

---
+++ src/main/java/com/svalero/tripalbumapi/TripalbumapiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.tripalbumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripalbumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripalbumapiApplication.class, args);
+	}
+
+}
