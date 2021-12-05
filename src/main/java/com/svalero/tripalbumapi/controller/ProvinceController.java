22801e1b6ed683e6497fe8b942b9803738500e0d
David de San Lázaro <david.dslazaro@gmail.com>
2021-12-05 16:59:23 +0100

    Trazabilidad

--- src/main/java/com/svalero/tripalbumapi/controller/ProvinceController.java
+++ src/main/java/com/svalero/tripalbumapi/controller/ProvinceController.java
@@ -8,6 +8,8 @@ import com.svalero.tripalbumapi.exception.ErrorResponse;
 import com.svalero.tripalbumapi.exception.ProvinceNotFoundException;
 import com.svalero.tripalbumapi.service.PlaceService;
 import com.svalero.tripalbumapi.service.ProvinceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import java.util.List;
 @RestController
 public class ProvinceController {
 
+    private final Logger logger = LoggerFactory.getLogger(ProvinceController.class);
+
     @Autowired
     private ProvinceService provinceService;
     @Autowired
@@ -25,53 +29,69 @@ public class ProvinceController {
 
     @GetMapping("/provinces")
     public List<Province> getProvinces() {
+        logger.info("Start getProvinces");
         List<Province> provinces;
 
         provinces = provinceService.findAllProvinces();
+        logger.info("End getProvinces");
         return provinces;
     }
 
     @GetMapping("/province/{id}")
     public Province getProvince(@PathVariable long id) throws ProvinceNotFoundException {
+        logger.info("Start ShowProvince " + id);
         Province province = provinceService.findProvince(id);
+        logger.info("End ShowProvince " + id);
         return province;
     }
 
     @DeleteMapping("/province/{id}")
     public Province removeProvince(@PathVariable long id) throws ProvinceNotFoundException {
+        logger.info("Start DeleteProvince " + id);
         Province province = provinceService.deleteProvince(id);
+        logger.info("End DeleteProvince " + id);
         return province;
     }
 
     @PostMapping("/provinces")
     public Province addProvince(@RequestBody ProvinceDTO provinceDto) throws CountryNotFoundException {
+        logger.info("Start AddProvince");
         Province province = provinceService.addProvince(provinceDto);
+        logger.info("End AddProvince");
         return province;
     }
 
     @PutMapping("/province/{id}")
     public Province modifyProvince(@RequestBody ProvinceDTO provinceDto, @PathVariable long id) throws ProvinceNotFoundException, CountryNotFoundException {
+        logger.info("Start ModifyProvince " + id);
         Province newProvince = provinceService.modifyProvince(id, provinceDto);
+        logger.info("End ModifyProvince " + id);
         return newProvince;
     }
 
     @GetMapping("/province/{provinceId}/places")
     public List<Place> getPlaces(@PathVariable long provinceId) throws ProvinceNotFoundException {
+        logger.info("Start getPlacesByProvince");
         List<Place> places = null;
+        logger.info("Search for province " + provinceId);
         Province province = provinceService.findProvince(provinceId);
+        logger.info("Province found. Search for places");
         places = placeService.findPlaces(province);
+        logger.info("End getPlacesByProvince");
         return places;
     }
 
     @ExceptionHandler(ProvinceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleProvinceNotFoundException(ProvinceNotFoundException pnfe) {
         ErrorResponse errorResponse = new ErrorResponse("1", pnfe.getMessage());
+        logger.info(pnfe.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CountryNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCountryNotFoundException(CountryNotFoundException cnfe) {
         ErrorResponse errorResponse = new ErrorResponse("1", cnfe.getMessage());
+        logger.info(cnfe.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
